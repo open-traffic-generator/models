2f3a2d18fee141194a02dd8df85dd9ec991abd68
Rangababu-R <72373312+Rangababu-R@users.noreply.github.com>
2021-08-27 11:28:32 +0530

    version upgrade

--- build.py
+++ build.py
@@ -9,8 +9,6 @@ import openapiart
 
 openapiart.OpenApiArt(
     api_files=["./api/info.yaml", "./api/api.yaml"],
-    # python_module_name="otg",
-    # protobuf_file_name="otg",
     protobuf_name="otg",
     artifact_dir="art",
 ).GeneratePythonSdk(package_name="otg")

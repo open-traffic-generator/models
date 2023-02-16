7e7aed5b8cc2ccc8b1623bc3cb89ec2c5a572a37
Ashutosh Kumar <ashutshkumr@gmail.com>
2023-02-16 21:24:45 +0000

    Squashed commit of the following:
    commit 3e65c80d6f07183e1664484e028c069fc01847d6
    Author: Ashutosh Kumar <ashutshkumr@gmail.com>
    Date:   Thu Feb 16 21:22:01 2023 +0000
    
        upgraded openapiart with version check support
    
    commit bf07c8c634ae224f16dcf6b2b8267c1f58e3e7fe
    Merge: 7ab90da e5b1413
    Author: Ashutosh Kumar <ashutshkumr@gmail.com>
    Date:   Thu Feb 16 20:10:19 2023 +0000
    
        Merge branch 'version-check' of https://github.com/open-traffic-generator/models into version-check
    
    commit 7ab90da6bde0471a372c7ec50c6eed979b7b3d49
    Author: Ashutosh Kumar <ashutshkumr@gmail.com>
    Date:   Thu Feb 16 20:09:47 2023 +0000
    
        cleanup
    
    commit e5b1413fc5aa30b40888e35034231deac15c5519
    Author: Github Actions Bot <actions@github.com>
    Date:   Thu Feb 16 12:12:14 2023 +0000
    
        Update auto generated content
    
    commit 5ec492707bbf54598d32e308d3fc409633367b70
    Author: Ashutosh Kumar <ashutshkumr@gmail.com>
    Date:   Thu Feb 16 12:10:31 2023 +0000
    
        added support for using private openapiart
    
    commit dead9d4bbeaff386aee3635ea6fb586d8467908c
    Author: Ashutosh Kumar <ashutshkumr@gmail.com>
    Date:   Thu Feb 16 11:53:33 2023 +0000
    
        fixed openapi version

--- build.py
+++ build.py
@@ -4,10 +4,32 @@ The following command produces these artifacts:
     - ./art/openapi.json
     - ./art/otg-convergence.proto
 """
+import sys
+import os
+import subprocess
+
+# supported values - local openapiart path or None
+USE_OPENAPIART_DIR = None
+
+# supported values - branch name or None
+USE_OPENAPIART_BRANCH = None
+
+OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
+
+
+if USE_OPENAPIART_DIR is not None:
+    sys.path.insert(0, USE_OPENAPIART_DIR)
+elif USE_OPENAPIART_BRANCH is not None:
+    local_path = "openapiart"
+    if not os.path.exists(local_path):
+        subprocess.check_call("git clone {} && cd {} && git checkout {} && cd ..".format(OPENAPIART_REPO, local_path, USE_OPENAPIART_BRANCH), shell=True)
+    sys.path.insert(0, local_path)
+
 import openapiart
 
 openapiart.OpenApiArt(
     api_files=["./api/info.yaml", "./api/api.yaml"],
     protobuf_name="otg",
     artifact_dir="artifacts",
+    generate_version_api=True,
 ).GeneratePythonSdk(package_name="otg")

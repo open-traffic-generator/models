7ccfeda030a36f6c734ea1f45e4703fe74a8dc43
vhowdhur <vibaswan.roychowdhury@keysight.com>
2024-05-08 09:55:22 +0530

    update to latest openapiart

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ import subprocess
 USE_OPENAPIART_DIR = None
 
 # supported values - branch name or None
-USE_OPENAPIART_BRANCH = "enhance_x_field_auto"
+USE_OPENAPIART_BRANCH = None
 
 OPENAPIART_REPO = "https://github.com/open-traffic-generator/openapiart.git"
 
